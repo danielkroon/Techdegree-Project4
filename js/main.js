5d36dde6a399cdf384a01086fe94290ec9c0fbd2
Daniel Kroon <drg.kroon@gmail.com>
2016-11-09 22:45:02 +0100

    Added simple lightbox

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ $('input').focus(function() {
 
 //CALL LIGHTBOX
 var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+var $caption = $('<p></p>');
+
+//add image to overlay
+$overlay.append($image);
+
+//add caption to overlay
+$overlay.append($caption);
 
 //add overlay
 $('body').append($overlay);
@@ -13,6 +21,16 @@ $('body').append($overlay);
 //capture click event on a image
 $('.grid a').click(function(event) {
     event.preventDefault();
-    var href = $(this).attr('href');
+    var imageLocation = $(this).attr('href');
+    //update overlay with image the image linked in the link
+    $image.attr("src", imageLocation);
     $overlay.show();
+    //get alt text from image
+    var captionText = $(this).attr('alt');
+    $caption.text(captionText);
+});
+
+
+$overlay.click(function() {
+    $(this).hide();
 });
