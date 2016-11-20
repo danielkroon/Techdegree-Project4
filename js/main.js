1e0fd62651ad2776a7f7c9ccfa371817d45e3197
Daniel Kroon <drg.kroon@gmail.com>
2016-11-20 18:07:30 +0100

    Added keyboard navigation

--- js/main.js
+++ js/main.js
@@ -1,31 +1,11 @@
-// R42 tag
-(function(a, d, e, b, f, c, s) {
-    a[b] = a[b] || function() {
-        a[b].q.push(arguments);
-    };
-    a[b].q = [];
-    c = d.createElement(e);
-    c.async = 1;
-    c.src = "//tdn.r42tag.com/lib/" + f + ".js";
-    s = d.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(c, s);
-})
-(window, document, "script", "_st", "1233-v1");
-// Add additional settings here (optional)
-_st("loadTags");
 
-
-//REMOVE PLACEHOLDER
-$('input').focus(function() {
-    $(this).removeAttr('placeholder');
-});
-
-//SEARCH GALLERY
-var search;
+/* ================================= 
+Search Filter
+==================================== */
 
 (function() {
     document.onkeyup = function() {
-        search = document.getElementById('search').value;
+        var search = document.getElementById('search').value;
 
         // Put all images in a var
         var images = document.getElementsByTagName('img');
@@ -34,18 +14,22 @@ var search;
         for (var i = 0; i < images.length; i++) {
             var alt = images[i].alt;
 
-            console.log('Does ' + search + ' match ' + alt + ' for this image?' + ' ' + (search == alt));
-			console.log('Does ' + alt + ' contain ' + search + ' for this image?' + ' ' + (alt.toLowerCase().indexOf(search.toLowerCase()) > -1));
+            // console.log('Does ' + search + ' match ' + alt + ' for this image?' + ' ' + (search == alt));
+			// console.log('Does ' + alt + ' contain ' + search + ' for this image?' + ' ' + (alt.toLowerCase().indexOf(search.toLowerCase()) > -1));
             if (alt.toLowerCase().indexOf(search.toLowerCase()) === -1) {
-                // images[i].style.display = 'block';
+                images[i].style.display = 'none';
             } else {
-                // images[i].style.display = 'block'; // eigen block
+                images[i].style.display = 'block';
             }
         }
     };
 })();
 
-//CALL LIGHTBOX
+
+/* ================================= 
+Lightbox
+==================================== */
+
 var $overlay = $('<div id="overlay"></div>');
 var $image = $('<img>');
 var $caption = $('<p></p>');
@@ -57,39 +41,52 @@ var $closeLightbox = $("<div id='closeLightbox'></div>");
 
 $overlay.append($image); //add image to overlay
 $overlay.append($caption); //add caption to overlay
-$overlay.append($buttonNext); //add next button to overlay
 $overlay.append($buttonPrevious); //add next button to overlay
 $overlay.append($closeLightbox);
+$overlay.append($buttonNext); //add next button to overlay
+
 
 $('body').append($overlay); //add overlay
 
-//capture click event on a image
+// Capture click event on a image
 $('.grid a').click(function(event) {
     event.preventDefault();
 
     getCurrentImage(this);
 
-    //Show the overlay
+    // Show the overlay
     $overlay.fadeIn(400);
 
 });
 
+// Lightbox arrow navigation
+$('body').keyup(function (e) {
+    if (e.keyCode==39) {
+        getNextImage();
+    } else if (e.keyCode==37) {
+        getPrevImage();
+    } else {
+		return;
+	}
+});
+
+// Lightbox click navigation
 $buttonNext.click(function() {
     getNextImage();
 });
 
 $buttonPrevious.click(function() {
     getPrevImage();
-
 });
 
+// Get images
 function getCurrentImage(currentImage) {
     thisImage = currentImage;
     var imageLocation = $(currentImage).attr('href');
-    //Update overlay with the image linked in the linked
+    // Update overlay with the image linked in the linked
     $image.attr('src', imageLocation);
 
-    //Get alt text from image
+    // Get alt text from image
     var captionText = $(currentImage).attr('alt');
     $caption.text(captionText);
 }
@@ -110,7 +107,7 @@ function getNextImage() {
     getCurrentImage(thisImage);
 }
 
-//Close overlay
+// Close overlay
 $closeLightbox.click(function() {
     $($overlay).fadeOut(400);
 });
